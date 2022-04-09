974ebc502cdec424b26e5ee7e89ac87c49275d46
hayashiyasuhito <hayashi.yasuhito.ta@alumni.tsukuba.ac.jp>
2022-04-09 16:16:52 +0900

    修正

--- TIS_serial.py
+++ TIS_serial.py
@@ -7,6 +7,8 @@ import time
 rpm=input("input rpm: ")
 rep=input("input rep: ")
 
+exp_min = input("input experimet time (min):")
+exp_time = int(exp_min)*60
 
 Tis = TIS.TIS()
 Tis.selectDevice("5220340",1920,1200, "10/1",TIS.SinkFormats.BGRA,True)
@@ -19,5 +21,11 @@ Tis.Set_Property("Exposure Auto", False)
 Tis.Set_Property("Exposure Time (us)",500)
 
 print("{},{},{},{}".format(Tis.Get_Property("Gain Auto").value,Tis.Get_Property("Gain").value,Tis.Get_Property("Exposure Auto").value,Tis.Get_Property("Exposure Time (us)").value))
+
+if int(rpm) == 47:
+    ShootingSpeed=
+    ShootingTime=0.7
+elif int(rpm) == 22:
+    ShootingSpeed="
 
 
